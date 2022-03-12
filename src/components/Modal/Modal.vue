4e52c9d3f7ec6f6423c47988c4e43483f683cf1b
jungai <juworaphol125@gmail.com>
2022-03-12 14:27:19 +0700

    add Modal Component

---
+++ src/components/Modal/Modal.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import { onMounted, onBeforeUnmount } from "vue";
+import { Card } from "../Card";
+
+const emit = defineEmits<{
+  (e: "close"): void;
+}>();
+
+const handleClickCloseBtn = () => {
+  emit("close");
+};
+
+onMounted(() => {
+  document.body.style.overflow = "hidden";
+});
+
+onBeforeUnmount(() => {
+  document.body.style.overflow = "visible";
+});
+</script>
+
+<template>
+  <div
+    z-50
+    fixed
+    flex
+    items-center
+    justify-center
+    pa1
+    top-0
+    left-0
+    h-screen
+    w-screen
+    touch-none
+    :style="{
+      backgroundColor: `rgba(0, 0, 0, 0.5)`,
+    }"
+  >
+    <div z-60 relative>
+      <Card w-300px>
+        <slot />
+      </Card>
+      <span
+        absolute
+        top="-10px"
+        right="-5px"
+        rounded-full
+        border="1px solid #ccc"
+        bg-white
+        flex
+        items-center
+        justify-center
+        h-25px
+        w-25px
+        cursor="pointer"
+        outline-none
+        @click="handleClickCloseBtn"
+      >
+        X
+      </span>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
