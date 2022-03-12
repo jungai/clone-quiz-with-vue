b4d7b78326a7265b60b320b01eda1d641fae3907
jungai <juworaphol125@gmail.com>
2022-03-12 14:27:08 +0700

    add Card Component

---
+++ src/components/Card/Card.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div overflow-hidden border-1 rounded-lg border-color="#ccc" h-280px bg-white>
+    <slot />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Card/CardImage.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const props = defineProps<{
+  src: string;
+  alt: string;
+}>();
+</script>
+
+<template>
+  <div h="60%">
+    <img
+      w-full
+      h-full
+      object-cover
+      :src="props.src"
+      :alt="props.alt"
+      loading="lazy"
+    />
+  </div>
+</template>
